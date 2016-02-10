533a41ad392d19b7d98d536bdb6803da014f038b
Daniel Futrillé <futridan@gmail.com>
2016-02-10 07:01:00 -0430

    Creacion de Consultas CRUD

--- src/Iglesys/Bundle/GanadosBundle/Controller/DefaultController.php
+++ src/Iglesys/Bundle/GanadosBundle/Controller/DefaultController.php
@@ -4,19 +4,52 @@ namespace Iglesys\Bundle\GanadosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
-    {
-        $key = $request->request->get('key',0);
-        return $this->render('IglesysGanadosBundle:Default:index.html.php');
+    public function getSerialize($object){
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        return $serializer->serialize($object, 'json');
     }
 
-    public function showAction(Request $request)
+    public function indexAction(Request $request)
     {
-        $key = $request->request->get('key',0);
-        return $this->render('IglesysGanadosBundle:Default:index.html.php');
+        $key = $request->query->get('key',0);
+
+        $vvaList = $this->getDoctrine()
+            ->getRepository('IglesysGeneralBundle:ValorVariable')
+            ->findAll();
+
+        $response = new JsonResponse();
+        $response->setContent($this->getSerialize($vvaList));
+        return $response;
+    }
+
+    public function showAction($id)
+    {
+        //$key = $request->query->get('key',0);
+        //$id = $request->request->get('id',0);
+        echo "ID:". $id;
+        $valorVariable = $this->getDoctrine()
+            ->getRepository('IglesysGeneralBundle:ValorVariable')
+            ->find($id);
+        if (!$valorVariable) {
+            throw $this->createNotFoundException('No se encuentra el registro solicitado.');
+        }
+
+        $response = new JsonResponse();
+        $response->setContent($this->getSerialize($valorVariable));
+        return $response;
     }
 
     public function updateAction(Request $request)
